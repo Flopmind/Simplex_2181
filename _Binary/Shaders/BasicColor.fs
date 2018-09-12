c817a62fc71c0530a1c38d6e266f79ee704ab36f
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-09-12 17:46:22 -0400

    finished exercise 3

--- _Binary/Shaders/BasicColor.fs
+++ _Binary/Shaders/BasicColor.fs
@@ -4,6 +4,8 @@ in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
 
+uniform bool Complement = false;
+
 out vec4 Fragment;
 
 void main()
@@ -11,7 +13,20 @@ void main()
 	Fragment = vec4(Color,1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
-		Fragment = vec4(SolidColor, 1);
+    {
+        if (Complement)
+        {
+            Fragment = vec4(1.0 - SolidColor.r, 1.0 - SolidColor.g, 1.0 - SolidColor.b, 1);
+        }
+        else
+        {
+            Fragment = vec4(SolidColor, 1);
+        }
+    }
+    else if(Complement)
+    {
+        Fragment = vec4(1.0 - Color.r, 1.0 - Color.g, 1.0 - Color.b, 1);
+    }
 
 	return;
 }
