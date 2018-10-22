004551feeb1e1add39590c216a0d9d29aeb38aa0
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-10-22 14:30:48 -0400

    e09 done

--- E09 - Projections/AppClass.cpp
+++ E09 - Projections/AppClass.cpp
@@ -3,7 +3,7 @@ using namespace Simplex;
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Michael Pasquarello - mjp8835@g.rit.edu";
 
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -53,22 +53,33 @@ void Application::Display(void)
 		m_pCamera->ResetCamera();
 		break;
 	case 2:
-		m_pCamera->ResetCamera();
+		//make orthographic
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * 15.0f, ZERO_V3, AXIS_Y);
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_X * -30.0f, ZERO_V3, -AXIS_Z);
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * -15.0f, ZERO_V3, AXIS_Y);
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
+		//make orthographic
+		//m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * -10.0f, ZERO_V3, AXIS_Y);
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
+		//make orthographic
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * -3.0f, AXIS_Z * -10.0f, AXIS_Y);
 		break;
 	case 7:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * 10.0f, ZERO_V3, -AXIS_Y);
 		break;
 	}
 
