0cd950aff8876ac094d59e8e632f5764f2cc2513
Michael Pasquarello (RIT Student) <mjp8835@ad.rit.edu>
2018-10-22 13:32:08 -0400

    fix

--- include/Simplex/Shaders/BasicColor.fs
+++ include/Simplex/Shaders/BasicColor.fs
@@ -4,8 +4,6 @@ in vec3 Color;
 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
 
-uniform bool Complement = false;
-
 out vec4 Fragment;
 
 void main()
@@ -13,20 +11,7 @@ void main()
 	Fragment = vec4(Color,1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
-    {
-        if (Complement)
-        {
-            Fragment = vec4(1.0 - SolidColor.r, 1.0 - SolidColor.g, 1.0 - SolidColor.b, 1);
-        }
-        else
-        {
-            Fragment = vec4(SolidColor, 1);
-        }
-    }
-    else if(Complement)
-    {
-        Fragment = vec4(1.0 - Color.r, 1.0 - Color.g, 1.0 - Color.b, 1);
-    }
+		Fragment = vec4(SolidColor, 1);
 
 	return;
 }
